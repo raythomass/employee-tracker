9f9704b73b95f955e3d55a4ba47ca18ab573691c
Ray Thomas <rthomas5678@gmail.com>
2023-11-14 16:10:59 -0700

    added schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INSERT role (title, salary, department_id)
+VALUES 
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+    (1, 'John', 'Doe', 1, NULL),
+    (2, 'Mike', 'Chan', 2, 1),
+    (3, 'Ashley', 'Rodriguez', 3, NULL),
+    (4, 'Kevin', 'Tupik', 4, 3),
+    (5, 'Kunal', 'Singh', 5, NULL),
+    (6, 'Malia', 'Brown', 6, 5),
+    (7, 'Sarah', 'Lourd', 7, NULL),
+    (8, 'Tom', 'Allen', 8, 7);
